b6ce10138891a25379dd887faa223a76653a7915
sowmya <sowmya.mathangi@infor.com>
2025-01-07 17:05:16 +0530

    First person commit

---
+++ src/test/java/Test1/Test1.java
@@ -0,0 +1,19 @@
+package Test1;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Test1 {
+	
+	public static void Test1()
+	{
+		WebDriver driver=new ChromeDriver();
+		driver.manage().window().maximize();
+
+
+	
+			System.out.println("Clicking");
+	}
+
+}
