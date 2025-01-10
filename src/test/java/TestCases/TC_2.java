f48083aa1c3d8f5316f7522b2c707280144c0150
sowmya <sowmya.mathangi@infor.com>
2025-01-10 18:45:48 +0530

    commit to git

---
+++ src/test/java/TestCases/TC_2.java
@@ -0,0 +1,17 @@
+package TestCases;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+
+public class TC_2 {
+
+	public static void main(String[] args) throws InterruptedException {
+
+		WebDriver driver=new ChromeDriver();
+		driver.manage().window().maximize();
+		
+	}
+}
